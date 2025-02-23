8fcdbb06a47786c39806d7fd5ac3e3a2f1c646a3
TLYordle <tranlam23072003@gmail.com>
2025-02-23 15:14:53 +0700

    Trần Văn Lâm
    Default

--- FinancialPlanning/src/main/java/com/group_2/FinancialPlanning/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group_2.FinancialPlanning.repository;
-
-import com.group_2.FinancialPlanning.entity.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission, String> {
-}
