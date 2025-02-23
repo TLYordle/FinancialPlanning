a69e7adaa15b72f2aad4f3e155aa77961aee3b3e
TLYordle <tranlam23072003@gmail.com>
2025-02-23 23:17:11 +0700

    Trần Văn Lâm

---
+++ target/generated-sources/annotations/com/group_2/FinancialPlanning/mapper/RoleMapperImpl.java
@@ -0,0 +1,75 @@
+package com.group_2.FinancialPlanning.mapper;
+
+import com.group_2.FinancialPlanning.dto.request.RoleRequest;
+import com.group_2.FinancialPlanning.dto.response.PermissionResponse;
+import com.group_2.FinancialPlanning.dto.response.RoleResponse;
+import com.group_2.FinancialPlanning.entity.Permission;
+import com.group_2.FinancialPlanning.entity.Role;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-02-23T23:08:25+0700",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
+)
+@Component
+public class RoleMapperImpl implements RoleMapper {
+
+    @Override
+    public Role toRole(RoleRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Role.RoleBuilder role = Role.builder();
+
+        role.name( request.getName() );
+        role.description( request.getDescription() );
+
+        return role.build();
+    }
+
+    @Override
+    public RoleResponse toRoleResponse(Role role) {
+        if ( role == null ) {
+            return null;
+        }
+
+        RoleResponse.RoleResponseBuilder roleResponse = RoleResponse.builder();
+
+        roleResponse.name( role.getName() );
+        roleResponse.description( role.getDescription() );
+        roleResponse.permissions( permissionSetToPermissionResponseSet( role.getPermissions() ) );
+
+        return roleResponse.build();
+    }
+
+    protected PermissionResponse permissionToPermissionResponse(Permission permission) {
+        if ( permission == null ) {
+            return null;
+        }
+
+        PermissionResponse.PermissionResponseBuilder permissionResponse = PermissionResponse.builder();
+
+        permissionResponse.name( permission.getName() );
+        permissionResponse.description( permission.getDescription() );
+
+        return permissionResponse.build();
+    }
+
+    protected Set<PermissionResponse> permissionSetToPermissionResponseSet(Set<Permission> set) {
+        if ( set == null ) {
+            return null;
+        }
+
+        Set<PermissionResponse> set1 = LinkedHashSet.newLinkedHashSet( set.size() );
+        for ( Permission permission : set ) {
+            set1.add( permissionToPermissionResponse( permission ) );
+        }
+
+        return set1;
+    }
+}
